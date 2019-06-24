fb4e198fc54610fe90a781fec0e13a7d6b35c2b6
leandra <leandra.reid@gmail.com>
2019-06-24 18:46:08 -0400

    adding local state to CandidateComments component to hold comment data

--- frontend/src/components/CandidateComments.js
+++ frontend/src/components/CandidateComments.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CandidateComments = () => (
-    <>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <button type="submit">Save comments</button>
-    </>
-);
+class CandidateComments extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            localComments : ""
+        }
+    };
+
+    handleChange (e){
+        this.setState({
+            localComments: e.target.value
+        });
+        // console.log(e.target.value)
+        console.log(this.state.localComments)
+    }
+    render(){
+        return (
+            <>
+                <textarea name="" id="" cols="30" rows="10" onChange={this.handleChange.bind(this)}></textarea>
+                <button type="submit">Save comments</button>
+            </>
+        )
+    }
+
+};
 
 export default CandidateComments;
